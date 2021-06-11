eecbc261914833cfe8c045e34cb4a34311bbb238
vZireael <75550025+vZireael@users.noreply.github.com>
2021-06-10 21:12:39 -0400

    added navbar, improved search bar, and added animations

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const movieArea = document.querySelector("#movie-list");
 const loadBtn = document.querySelector("#load-btn");
 const searchBtn = document.querySelector(".fa-search");
+const nowPlaying = document.querySelector(".now-playing");
 // prep for modal popup
 const form = document.querySelector("form");
 const modal = document.querySelector(".modal");
@@ -37,8 +38,15 @@ form.addEventListener("submit", (evt) => {
   evt.preventDefault();
   clearHMTL();
   searchName = evt.target.movieTitle.value;
-  console.log(searchName);
-  endPoint = "search/movie";
+  if (!searchName) {
+    endPoint = "movie/now_playing";
+    page = 1;
+    nowPlaying.innerHTML = "Now Playing";
+  } else {
+    endPoint = "search/movie";
+    nowPlaying.innerHTML = "Search Results";
+  }
+
   getApiData();
 });
 
@@ -56,10 +64,18 @@ async function getApiData() {
 }
 
 function displayMovies(results) {
+  let posterPath;
   results.forEach((element, index) => {
+    // displays coming soon image if movie does not have a poster, else displays poster.
+    if (element.poster_path == null) {
+      posterPath = "./images/comingSoon.jpeg";
+    } else {
+      posterPath = `https://image.tmdb.org/t/p/original${element.poster_path}`;
+    }
+
     movieArea.innerHTML += `
     <div class="card">
-      <img class="poster" src="https://image.tmdb.org/t/p/original${element.poster_path}" alt="${element.title} poster image" />
+      <img class="poster" src="${posterPath}" alt="${element.title} poster image" />
       <div class="info">
         <span class="movie-name">${element.title}</span>
         <span class="rating"><i class="fas fa-star"></i>${element.vote_average}</span>
